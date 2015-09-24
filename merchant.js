465b60db48e885267a7969f5a918816449d8c39a
li8 <srivastava.suhas@gmail.com>
2015-09-24 17:13:07 +0530

    Working version of mercahnt

--- merchant.js
+++ merchant.js
@@ -1,35 +1,32 @@
 if (Meteor.isClient) {
-  Session.get('customerId','randomTxt')
+  Session.get('consumerId','randomTxt')
 
   /* ReadNFC START */
 
-  Template.readNfc.helpers({
-    wittyNfcListner: function () {
+  // Template.readNfc.helpers({
+    wittyNfcListener= function () {
+      console.log("Listener init");
       // debugger;
-      // nfc.addNdefListener (
-      nfc.addTagDiscoveredListener (
+      nfc.addNdefListener (
+      // nfc.addTagDiscoveredListener (
         function (nfcEvent) {
             var tag = nfcEvent.tag,
             ndefMessage = tag.ndefMessage;
+            consumerId = nfc.bytesToString(ndefMessage[0].payload).substring(3);
 
-            // dump the raw json of the message
-            // note: real code will need to decode
-            // the payload from each record
-            // assuming the first record in the message has 
-            // a payload that can be converted to a string.
-            // ASSUMING : that is the customer Id
-            customerId = nfc.bytesToString(ndefMessage[0].payload).substring(3);
-            if(!customerId){
+            if(!consumerId){
               alert('Couldnt Load Customer Id. \n Please try again ');
               return false
+            }else{
+              consumerId = consumerId.split(':')[1];
+              Session.set('consumerId',consumerId);
+              $('#readNfc').fadeOut(600);
+              $('#paymentPage').fadeIn(1200);
+
             }
 
-            // set Sesssion.set 'customerId'
-            Session.set('customerId',customerId);
-            $('#readNfc').fadeOut(600);
-            $('#paymentPage').fadeIn(1200);
 
-        }, 
+        },  
         function () { // success callback
             alert("Waiting for NDEF tag");
         },
@@ -39,13 +36,15 @@ if (Meteor.isClient) {
       );
       return Session.get('counter');
     }
-  });
+  // });
 
  
   Template.readNfc.events({
     'click button': function () {
       // increment the counter when button is clicked
-      Session.get('wittyNfcListener');
+      console.log("readNfc Called");
+      // Session.get('wittyNfcListener');
+      wittyNfcListener();
     }
   });
   /*ReadNFC End */
@@ -53,8 +52,8 @@ if (Meteor.isClient) {
   /*PaymentPage Start */
   Template.paymentPage.helpers({
     customer:  function () {
-      if(Session.get('customerId')){
-        return Session.get('customerId');
+      if(Session.get('consumerId')){
+        return Session.get('consumerId');
       return ;
       }else{
         $('#paymentPage').fadeOut(600);
@@ -68,9 +67,13 @@ if (Meteor.isClient) {
     'click button': function () {
       // write code to execute the payment 
 
-      //get value of amount
-      // TODO: Validate
-      amount = $('#amount').val();
+      data ={};
+      data.amount = $('#amount').val();
+      data.consumer_id = Session.get('consumerId');
+      Meteor.call('pay',data,function(err,res){
+        alert("Result Msg:",res.message);
+      });
+
       // write function to call payment method with basic auth
     }
   });
@@ -132,3 +135,31 @@ if (Meteor.isCordova) {
   })
 }
 
+if(Meteor.isServer){
+    Meteor.methods({
+      pay: function(data) {
+        console.log("Pay Init");
+        consumer_id= data.consumer_id || undefined;
+        if (!consumer_id) {return false}
+        amount= data.amount || 0.01;
+
+        var respJSON, results;
+        results = Meteor.http.post('http://180.179.146.81/wallet/v1/debits/general', {
+          data: {
+            'consumer_id': consumer_id,
+            'total_amount': amount,
+            'email': "sri@witworks.com",
+            'merchant_refID': "test13" + Math.floor((Math.random() * 1000) + 1)
+          },
+          headers: {
+            'Authorization': 'Basic NDBhY2U3OWQwMDUxMzc1MDhkZDQyYWQzNjdjM2RjMDg6Y2MxZTcyNzBkN2JkNTUwYzNiNjQ0YzczYTk1NWZlMjk='
+          }
+        });
+        respJSON = JSON.parse(results.content);
+        console.log(respJSON);
+        return respJSON;
+      }
+  });
+
+
+}
---
+++ package.js
@@ -0,0 +1,30 @@
+// // weinre --boundHost 192.168.2.5
+// //  # add nfc
+// // meteor add cordova:com.chariotsolutions.nfc.plugin@0.5.1
+// //  # doesnt work
+// // # meteor add cordova:com.chariotsolutions.nfc.plugin@https://github.com/jalbersol/phonegap-nfc/tarball/ebfcd23bc5bba01d9b9529f0fbe528894e6bc
+
+// // cordova.exec paytm ---> executes java class paytm
+
+// // find how to get merchant id 
+
+// /* Information about this package */
+// Package.describe({
+//   // Short two-sentence summary.
+//   summary: "Witworks NFC merchant end",
+//   // Version number.
+//   version: "1.0.0",
+//   name: "li8:wittymerc",
+//   // Optional github URL to your source repository.
+//   // git: "https://github.com/something/something.git",
+// });
+
+
+// Package.onUse(function(api) {
+//     api.versionsFrom('0.9.0');
+//   api.addAsset('main_logo.png', 'web.cordova');
+//   // api.addAssets('../../packages.json', 'server', {
+//   //   // isAsset: true
+//   //   addAsset:true
+//   // });
+// });
