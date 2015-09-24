aef104c01df4fb9ad1be9e8a44ca807d0ef61300
li8 <srivastava.suhas@gmail.com>
2015-09-24 21:55:57 +0530

    Change a

--- merchant.js
+++ merchant.js
@@ -1,42 +1,41 @@
 if (Meteor.isClient) {
-  Session.get('consumerId','randomTxt')
+  Session.set('consumerId','rndmTxt');
+  Session.set('amount',0.1);
 
   /* ReadNFC START */
 
-  // Template.readNfc.helpers({
-    wittyNfcListener= function () {
-      console.log("Listener init");
-      // debugger;
-      nfc.addNdefListener (
-      // nfc.addTagDiscoveredListener (
-        function (nfcEvent) {
-            var tag = nfcEvent.tag,
-            ndefMessage = tag.ndefMessage;
-            consumerId = nfc.bytesToString(ndefMessage[0].payload).substring(3);
+  wittyNfcListener= function () {
+    console.log("Listener init");
+    nfc.addNdefListener (
+      function (nfcEvent) {
+          var tag = nfcEvent.tag,
+          ndefMessage = tag.ndefMessage;
+          consumerId = nfc.bytesToString(ndefMessage[0].payload).substring(3);
 
-            if(!consumerId){
-              alert('Couldnt Load Customer Id. \n Please try again ');
-              return false
-            }else{
-              consumerId = consumerId.split(':')[1];
-              Session.set('consumerId',consumerId);
-              $('#readNfc').fadeOut(600);
-              $('#paymentPage').fadeIn(1200);
+          if(!consumerId){
+            alert('Couldnt Load Customer Id. \n Please try again ');
+            return false
+          }else{
+            consumerId = parseInt(consumerId.split(':')[1],10);
+            console.log("New ConsumerID", consumerId)
+            Session.set('consumerId',consumerId);
+            console.log("Session ConsumerID", Session.get('consumerId'));
+            $('#readNfc').fadeOut(600);
+            $('#paymentPage').fadeIn(1200);
 
-            }
+          }
 
 
-        },  
-        function () { // success callback
-            alert("Waiting for NDEF tag");
-        },
-        function (error) { // error callback
-            alert("Error adding NDEF listener " + JSON.stringify(error));
-        }
-      );
-      return Session.get('counter');
-    }
-  // });
+      },  
+      function () { // success callback
+          alert("Waiting for NDEF tag");
+      },
+      function (error) { // error callback
+          alert("Error adding NDEF listener " + JSON.stringify(error));
+      }
+    );
+    return true;
+  }
 
  
   Template.readNfc.events({
@@ -44,6 +43,13 @@ if (Meteor.isClient) {
       // increment the counter when button is clicked
       console.log("readNfc Called");
       // Session.get('wittyNfcListener');
+      amount = $('#amount').val();
+      filter = /^[0-9]*$/;
+      if(filter.test(amount)){
+        alert('Please enter a valid no');
+        return false;
+      }
+      Session.set('amount',amount);
       wittyNfcListener();
     }
   });
@@ -51,33 +57,77 @@ if (Meteor.isClient) {
 
   /*PaymentPage Start */
   Template.paymentPage.helpers({
-    customer:  function () {
-      if(Session.get('consumerId')){
-        return Session.get('consumerId');
-      return ;
-      }else{
-        $('#paymentPage').fadeOut(600);
-        $('#readNfc').fadeIn(1200);        
-        return 0
-      }
+    amount:  function () {
+      return Session.get('amount');
     }
   });
 
   Template.paymentPage.events({
     'click button': function () {
       // write code to execute the payment 
-
-      data ={};
-      data.amount = $('#amount').val();
-      data.consumer_id = Session.get('consumerId');
-      Meteor.call('pay',data,function(err,res){
-        alert("Result Msg:",res.message);
-      });
-
-      // write function to call payment method with basic auth
+      // window.pay(Session.get(amount)
     }
   });
   /*PaymentPage End */
+
+  window.pay= function(){
+      console.log("Pay fn ");
+      console.log("Ver 1");
+      data ={};
+      data.amount = Session.get('amount');
+      data.consumer_id = Session.get('consumerId');
+      // TODO later shift to server
+      Meteor.call('pay',data,function(err,res){
+        console.log(err);
+        console.log("Result Msg:",res.message);
+      });
+      console.log("data loaded", Session.get('consumerId'),Session.get('amount'));
+       // data= {
+       //      'consumer_id': Session.get('consumerId'),
+       //      'total_amount': Session.get('amount'),
+       //      'email': "sri@witworks.com",
+       //      'merchant_refID': "test13" + Math.floor((Math.random() * 1000) + 1)
+       //  };
+       //  console.log("Starting Payment");
+       //  console.log("Payment Obj : ", data);
+       //  results = Meteor.http.post('http://180.179.146.81/wallet/v1/debits/general', {
+       //    data: {
+       //      'consumer_id': consumer_id,
+       //      'total_amount': amount,
+       //      'email': "sri@witworks.com",
+       //      'merchant_refID': "test13" + Math.floor((Math.random() * 1000) + 1)
+       //    },
+       //    headers: {
+       //      'Authorization': 'Basic NDBhY2U3OWQwMDUxMzc1MDhkZDQyYWQzNjdjM2RjMDg6Y2MxZTcyNzBkN2JkNTUwYzNiNjQ0YzczYTk1NWZlMjk='
+       //    }
+       //  });
+       //  console.log("Payment return called");
+       //  console.log(results);
+       //  respJSON = JSON.parse(results.content);
+       //  console.log(respJSON);
+       //  return respJSON;
+
+      // $.ajax({
+      //   url: "http://180.179.146.81/wallet/v1/debits/general",
+      //   data:data,
+      //   method:'POST',
+      //   type:"application/json",
+      //   beforeSend: function (xhr) {
+      //       xhr.setRequestHeader ("Authorization", 'Basic NDBhY2U3OWQwMDUxMzc1MDhkZDQyYWQzNjdjM2RjMDg6Y2MxZTcyNzBkN2JkNTUwYzNiNjQ0YzczYTk1NWZlMjk=');
+      //   },
+      //   success:function(res) {
+      //     console.log(res);
+      //     alert(res);
+      //   },
+      //   error:function(err){
+      //     console.error(err);
+      //     alert("Payment Failed");
+      //   }
+      // });
+      // console.log("Payment return called");
+      // return true
+      // write function to call payment method with basic auth
+  }
 
 }
 
@@ -130,7 +180,7 @@ if (Meteor.isCordova) {
     Cordova.depends({
       'com.chariotsolutions.nfc.plugin':'https://github.com/jalbersol/phonegap-nfc/tarball/ebfcd23bc5bba01d9b9529f0fbe528894e6bca64'
     });
-
+    
 
   })
 }
